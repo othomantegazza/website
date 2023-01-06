7092feac50cbf5c2185973f0c2dba58536e1bf66
Otho Mantegazza <otho.mantegazza@gmail.com>
2023-01-06 17:11:44 +0100

    highlight nested structure with indents

--- R/make-content-component.R
+++ R/make-content-component.R
@@ -5,29 +5,28 @@ make_project_comp <- function(proj_data, proj_title) {
   
   cat(
     '<div class="grid">',
-    '<div class="g-col-12 g-col-sm-6 g-col-xl-4 project-cover}">',
-    '<video autoplay muted loop>',
-    glue::glue('<source src="{proj$image_path}" type="video/webm">'),
-    '</video>',
+      '<div class="g-col-12 g-col-sm-6 g-col-xl-4 project-cover}">',
+      '<video autoplay muted loop>',
+      glue::glue('<source src="{proj$image_path}" type="video/webm">'),
+      '</video>',
     # glue('{{< video  >}}'),
     '</div>',
     '<div class=" g-col-12 g-col-sm-6 g-col-xl-8">',
-    '<div class="grid">',
-    '<div class="g-col-12 g-col-xl-6 proj-info">',
-    glue::glue(
-      "**Year**: {proj$info$year}",
-      "**Place**: {proj$info$place}",
-      "**Stack**: {proj$info$stack}",
-      "**URL**: {proj$info$url}",
-      "**Source Code**: {proj$info$source}",
-      .sep = "<br>"
-    ),
-    '</div>',
-    '<div class="g-col-12 g-col-xl-6 proj-description">',
-    glue::glue("**Description**: ", proj$description$content),
-    '</div>',
-    '</div>',
-    '</div>',
+      '<div class="grid">',
+        '<div class="g-col-12 g-col-xl-6 proj-info">',
+          glue::glue(
+            "**Year**: {proj$info$year}",
+            "**Place**: {proj$info$place}",
+            "**Stack**: {proj$info$stack}",
+            "**URL**: {proj$info$url}",
+            "**Source Code**: {proj$info$source}",
+            .sep = "<br>"
+          ),
+        '</div>',
+        '<div class="g-col-12 g-col-xl-6 proj-description">',
+          glue::glue("**Description**: ", proj$description$content),
+        '</div>',
+      '</div>',
     '</div>',
     sep = "\n"
   )
