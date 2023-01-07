003a81358a217c77b8d4f38133f950a0beeb8662
Otho Mantegazza <otho.mantegazza@gmail.com>
2023-01-07 18:27:31 +0100

    project cover can be also video

--- R/make-content-component.R
+++ R/make-content-component.R
@@ -1,32 +1,46 @@
 # <img src="projects/_project.png" alt="A picture of the project" class="project-img"/>
-make_project_comp <- function(proj_yaml) {
+make_project_comp <- function(proj_yaml, vid_cover = FALSE) {
   
   proj <- yaml::read_yaml(proj_yaml)
   
-  cat(
-    '<div class="grid" style="--bs-columns: 6;">',
-      '<div class="g-col-6 g-col-xl-3 project-cover">',
-      '<video autoplay muted loop>',
-      glue::glue('<source src="{proj$image_path}" type="video/webm">'),
-      '</video>',
-    '</div>',
-    '<div class="g-col-6 g-col-xl-3">',
-      '<div class="grid" style="--bs-columns: 2;">',
-        '<div class="g-col-2 g-col-sm-1 proj-description">',
-          glue::glue(
-            "**Year**: {proj$year}",
-            "**Place**: {proj$place}",
-            "**Stack**: {proj$stack}",
-            "**Project URL**: {proj$url}",
-            "**Source Code**: {proj$source}",
-            .sep = "<br>"
-          ),
+  sep <- "\n"
+  
+  out <- 
+    glue::glue(
+      '<div class="grid" style="--bs-columns: 6;">',
+        '<div class="g-col-6 g-col-xl-3 project-cover">',
+          if(vid_cover) {
+            glue::glue(
+            '<video autoplay muted loop>',
+            glue::glue('<source src="{proj$video_path}" type="video/webm">'),
+            '</video>'
+            )
+          } else {
+            glue::glue(
+              '<img img src="{proj$image_path}" alt="Project image">'
+            )
+          },
         '</div>',
-        '<div class="g-col-2 g-col-sm-1 proj-description">',
-          glue::glue("**Description**: ", proj$description),
+        '<div class="g-col-6 g-col-xl-3">',
+          '<div class="grid" style="--bs-columns: 2;">',
+            '<div class="g-col-2 g-col-sm-1 proj-description">',
+              glue::glue(
+                "**Year**: {proj$year}",
+                "**Place**: {proj$place}",
+                "**Stack**: {proj$stack}",
+                "**Project URL**: {proj$url}",
+                "**Source Code**: {proj$source}",
+                .sep = "<br>"
+              ),
+            '</div>',
+            '<div class="g-col-2 g-col-sm-1 proj-description">',
+              glue::glue("**Description**: ", proj$description),
+            '</div>',
+          '</div>',
         '</div>',
       '</div>',
-    '</div>',
-    sep = "\n"
-  )
+      sep = "\n"
+    )
+  
+  cat(out)
 }
