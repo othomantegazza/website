b33040bd9eac6d10c0e0a895647a20e2e303cdb0
Otho Mantegazza <otho.mantegazza@gmail.com>
2023-03-14 21:38:00 +0100

    fix title margin in book include

--- R/pre-render.R
+++ R/pre-render.R
@@ -3,4 +3,20 @@ if( ! Sys.getenv("QUARTO_PROJECT_RENDER_ALL") == "1" && ! interactive()) {
   quit()
 }
 
-system('curl https://raw.githubusercontent.com/othomantegazza/data-and-sci-open-books/main/README.md > data/books.qmd')
+books_path <- here::here('data/books.qmd')
+
+books_url <-   
+  glue::glue(
+    'https://raw.githubusercontent.com/othomantegazza/',
+    'data-and-sci-open-books/main/README.md',
+  )
+
+system(
+  glue::glue(
+    'curl {books_url} > {books_path}'
+  )
+)
+
+system(
+  glue::glue('sed -i -e 1,2d {books_path}')
+)
